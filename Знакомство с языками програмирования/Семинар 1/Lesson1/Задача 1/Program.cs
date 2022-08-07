1c2e81135ef048882776b7f26bcff9da3c42a0d2
Olga <olgaharceva946@gmail.com>
2022-08-07 17:17:38 +0300

    add Решение первой задачи

---
+++ Знакомство с языками програмирования/Семинар 1/Lesson1/Задача 1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+// a = 5; b = 25 -> да 
+// a = 2 b = 10 -> нет 
+// a = 9; b = -3 -> нет 
+// a = -3 b = 9 -> да
+
+int Prompt(string messege)
+{
+    System.Console.Write(messege);           //Ввыводит сообщение
+    string readValue = Console.ReadLine();   // Считывает с консоли строку
+    int result = int.Parse(readValue);       // Преобразует строку в целое число
+    return result;                           // Возвращает результат
+}
+
+int numberOne = Prompt("Введите первое число: ");
+int numberTwo = Prompt("Введите второе число: ");
+
+int result = numberOne * numberOne;
+if(result == numberTwo)
+{
+    System.Console.WriteLine($"Число {numberTwo} является квадратом числа {numberOne}");
+}
+
+else
+{
+    System.Console.WriteLine($"Число {numberTwo} не является квадраточ числа {numberOne}");
+}
+
