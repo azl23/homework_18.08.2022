7b7c32382a6b9807f493aed473fb40a205198c03
Olga <olgaharceva946@gmail.com>
2022-08-22 16:58:46 +0300

    add Homework

---
+++ Знакомство с языками програмирования/Семинары/Lesson4/Домашняя работа/Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+
+int ExponentiationOfNumber(int value1, int value2)
+{
+    int result = 1;
+    for (int i = 0; i < value2; i++)
+    {
+        result = result * value1;
+    }
+    return result;
+}
+
+int numberOne = Prompt("Введите первое значение -> ");
+int numberTwo = Prompt("Введите второе значение -> ");
+if (numberTwo < 0)
+{
+    Console.WriteLine($"Неккоректный ввод");
+}
+else
+{
+    Console.WriteLine($"{numberOne}, {numberTwo} -> {ExponentiationOfNumber(numberOne, numberTwo)}");
+}
---
+++ Знакомство с языками програмирования/Семинары/Lesson4/Домашняя работа/Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+
+int SumOfDigitsOfNumber(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result = result + number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+int value = Prompt("Введите значение ->");
+if (value >= 0)
+{
+    System.Console.WriteLine($"{value} -> {SumOfDigitsOfNumber(value)}");
+}
+else
+{
+    System.Console.WriteLine("Неккоректный ввод, введите положительное число ");
+}
